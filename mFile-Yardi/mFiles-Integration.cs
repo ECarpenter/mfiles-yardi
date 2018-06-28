27f0eff2650ca6d91e1e13a97fd4337acddb845d
= <=>
2018-06-28 09:29:53 -0700

    Real outline, other wasn't saved

--- mFile-Yardi/mFiles-Integration.cs
+++ mFile-Yardi/mFiles-Integration.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace mFile_Yardi
 {
-    class Class1
+    class export
     {
+        //
+        //
+        // pseudo code for pulling invoice data out of mFiles
+        //
+        //
+
+        //connect to mFiles
+
+        //open vault
+
+        //pull up invoices
+
+        //loop through invoices collecting at import workflow state
+
+            //add data to xml object
+
+            //change workflow state 
+        
+        //End Loop
+
+        //combine xaml objects
+
+        //save export file
+    }
+
+    class import
+    {
+        //
+        //
+        //Pseudo code for importing check data from yardi
+        //
+        // 
+
+
+        //connect to mFiles
+
+        //open vault
+
+        //open XML file
+
+        //loop through items
+
+            //find mFiles object with matching invoice number
+
+            //add check number to mFiles Object
+
+            //change state to complete
+
+        //End Loop
+
     }
 }
